9130a5d51af701892c8cbcf01d6b0d6c5c100a63
cbenitez99 <cbenitezbusiness9@gmail.com>
2021-06-18 13:32:21 -0500

    commit tree 8f574b585ef8f892edaca3cfd427a44c9a81ec0c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+// function findMatching(nameArr, string){
+//     return nameArr.filter(name => name.toLowerCase() === string.toLowerCase())
+// }
+
+// function fuzzyMatch(driver, string){
+//     return driver.filter(name => name.toLowerCase().indexOf(string.toLowerCase()) === 0)
+// }
+
+// function matchName(driver, string){
+//     return driver.filter(names => names.name === string)
+// } 
+
